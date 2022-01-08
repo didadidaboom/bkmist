9eedd9903ee298b678ef17410dc3e78c5a88b934
WuChong <chong.wu.sg@outlook.com>
2022-01-09 05:42:42 +0800

    address moment updated 7

--- Ant_api/api/serializer/address.py
+++ Ant_api/api/serializer/address.py
@@ -65,6 +65,6 @@ class GetAddressMomentModelSerializer(ModelSerializer):
             moment["share_count"] = item["moment__share_count"]
             moment["if_status"] = item["moment__if_status"]
             moment["moment_status"] = item["moment__moment_status"]
-            moment_list[item["id"]]=moment
+            moment_list[item["moment_id"]]=moment
         return moment_list.values()
 
