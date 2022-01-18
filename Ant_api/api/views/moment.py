ba48f423cfff0ca75334dbac7085a98d3d59c6d4
WuChong <chong.wu.sg@outlook.com>
2022-01-19 00:49:18 +0800

    update focus part

--- Ant_api/api/views/moment.py
+++ Ant_api/api/views/moment.py
@@ -62,7 +62,7 @@ class FocusMomentView(ListAPIView):
         ).filter(
             Q(user__user_focus__focus_user_id=self.request.user.id)
         ).filter(Q(if_status=0)|Q(favor_count__gt = settings.MAX_FAVOR_COUNT_IF_STATUS)|
-                 Q(Q(if_status=1)&Q(user__user_focus__focus_user_focus_count__gt=3))
+                 Q(Q(if_status=1)&Q(user__user_focus__focus_user__focus_count__gt=3))
         ).all().distinct().order_by('-id')
         return queryset
 
