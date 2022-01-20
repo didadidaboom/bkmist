91718bb673838be00ceb9977e3c16b583ed60179
WuChong <chong.wu.sg@outlook.com>
2022-01-21 00:42:50 +0800

    show comment in my own view

--- Ant_api/api/serializer/comment.py
+++ Ant_api/api/serializer/comment.py
@@ -36,10 +36,10 @@ class CreateCommentSerializer(ModelSerializer):
 
     def get_reply__nickName(self,obj):
         request = self.context.get("request")
-        com_obj = models.CommentRecord.objects.filter(id=obj.id).first()
-        if com_obj.reply.user.id == request.user.id:
-            return com_obj.reply.nickName+('我')
-        return com_obj.reply.nickName
+        com_obj = models.CommentRecord.objects.filter(id=obj.reply.id).first()
+        if com_obj.user.id == request.user.id:
+            return com_obj.nickName+('我')
+        return com_obj.nickName
 
     def get_status(self, obj):
         '''
