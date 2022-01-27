9afaa6e0eec38dd55d723e40b81d3d93f78ba428
WuChong <chong.wu.sg@outlook.com>
2022-01-27 15:39:44 +0800

    manager

--- Ant_api/api/views/manage.py
+++ Ant_api/api/views/manage.py
@@ -5,18 +5,18 @@ from api.serializer import manage
 
 from utils import pagination,filter,auth
 
-class getAllOpenidUsedListView(ListAPIView):
+class getAllChongOpenidUsedListView(ListAPIView):
     serializer_class = manage.getAllOpenidUsedListModelSerializer
     pagination_class = pagination.Pagination
     filter_backends = [filter.MinFilterBackend, filter.MaxFilterBackend]
     authentication_classes = [auth.GeneralAuthentication, ]
 
     def get_queryset(self):
-        queryset = models.UserInfo.objects.all().order_by("-id")
+        queryset = models.UserInfo.objects.filter(openID__startswith="oCKHr4gWMcH8ql0MPh7eE74llRpc").all().order_by("-id")
         return queryset
 
 
-class UpdateopenidView(UpdateAPIView):
+class UpdateChongopenidView(UpdateAPIView):
     queryset = models.UserInfo.objects
     serializer_class = manage.UpdateopenidModelSerializer
 
