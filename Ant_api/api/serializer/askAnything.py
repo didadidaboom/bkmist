20ecf5deeb1cd67154dbdbfb18a7069e7e83f50a
WuChong <chong.wu.sg@outlook.com>
2022-02-03 04:38:54 +0800

    modify tacit

--- Ant_api/api/serializer/askAnything.py
+++ Ant_api/api/serializer/askAnything.py
@@ -1,5 +1,8 @@
+from math import floor,ceil
+
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from django.conf import settings
 
 from api import models
 
@@ -9,3 +12,78 @@ class CreateAskAnythingModelSerializer(ModelSerializer):
     class Meta:
         model = models.TacitRecord
         fields = ["id","type","user_id"]
+
+class SubmitAskAnythingModelSerializer(ModelSerializer):
+    user_id = serializers.IntegerField(source="user.id",read_only=True)
+    #user__nickName = serializers.CharField(source="nickName" ,read_only=True)
+    #user__avatarUrl = serializers.CharField(source="avatarUrl",read_only=True)
+    #user__nickName = serializers.SerializerMethodField(read_only=True)
+    #user__avatarUrl = serializers.SerializerMethodField(read_only=True)
+    reply_id = serializers.IntegerField(source="reply.id",read_only=True)
+    reply__user_id = serializers.IntegerField(source="reply.user.id",read_only=True)
+    #reply__nickName = serializers.CharField(source="reply.nickName",read_only=True)
+    reply__nickName = serializers.SerializerMethodField(read_only=True)
+    #create_date = serializers.DateTimeField(format=("%Y-%m-%d %H:%M:%S"),read_only=True)
+    create_date = serializers.SerializerMethodField()
+    root_id = serializers.IntegerField(source="root.id",read_only=True)
+    favor_count = serializers.IntegerField(read_only=True)
+    status = serializers.SerializerMethodField(read_only=True)
+    nickName = serializers.SerializerMethodField()
+
+    class Meta:
+        model = models.AskAnythingRecord
+        exclude = ["user"]
+
+    def get_nickName(self,obj):
+        request = self.context.get("request")
+        if obj.user.id == request.user.id:
+            return obj.nickName+'(我)'
+        return obj.nickName
+
+    def get_reply__nickName(self,obj):
+        request = self.context.get("request")
+        if not obj.reply:
+            return None
+        if obj.reply.user.id == request.user.id:
+            return obj.reply.nickName+('(我)')
+        return obj.reply.nickName
+
+    def get_status(self, obj):
+        '''
+        1.判断评论者是否为瞬间发布本人：如果不是评论区状态显示 根据评论状态而定
+        2.如果是本人，判断瞬间发布的状态
+        3.如果状态为公开0，评论区状态显示 根据评论状态而定
+        4.如果状态为条件隐身，评论区状态根据瞬间状态而定，如果瞬间受欢迎达到上线，评论区楼主状态 根据评论状态而定
+        '''
+        if obj.tacitrecord.user.id != obj.user.id:
+            if obj.comment_status==0:
+                return {"comment_status_user_id":obj.user.id,"comment_status_name":None}
+            if obj.favor_count < settings.MAX_FAVOR_COUNT_IF_STATUS_COMMENT:
+                return {"comment_status_user_id":None,"comment_status_name":"条"}
+            return {"comment_status_user_id":obj.user.id,"comment_status_name":"裂"}
+        else:
+            return {"comment_status_user_id": obj.user.id, "comment_status_name": None}
+    def get_create_date(self,obj):
+        create_date = obj.create_date
+        a = create_date
+        b = create_date.now()
+        delta = b - a
+        second = delta.seconds
+        minute_ori = second / 60
+        minute_ceil = ceil(minute_ori)
+        minute_floor = floor(minute_ori)
+        hour_ori = minute_ori / 60
+        hour_ceil = ceil(hour_ori)
+        hour_floor = floor(hour_ori)
+        day_ori = delta.days
+        day = day_ori
+        if (day_ori):
+            return str(day) + "天前"
+        else:
+            if (hour_ori > 1):
+                return str(hour_floor) + "小时前"
+            else:
+                if (minute_ori > 1):
+                    return str(minute_floor) + "分钟前"
+                else:
+                    return str(second) + "秒前"
--- Ant_api/api/views/askAnything.py
+++ Ant_api/api/views/askAnything.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import CreateAPIView
+from django.db.models import F
 
 from api.serializer import askAnything
+from api import models
 from utils.auth import UserAuthentication
+from utils.randomName import getNameAvatarlist,getMosaic,getRandomName,getRandomAvatar
 
 class CreateAskAnythingView(CreateAPIView):
     serializer_class = askAnything.CreateAskAnythingModelSerializer
@@ -12,9 +15,49 @@ class CreateAskAnythingView(CreateAPIView):
         return obj
 
 class SubmitAskAnythingView(CreateAPIView):
-    serializer_class = askAnything.CreateAskAnythingModelSerializer
+    '''保存评论 同时更新瞬间里面的评论数'''
+    serializer_class = askAnything.SubmitAskAnythingModelSerializer
     authentication_classes = [UserAuthentication,]
 
     def perform_create(self, serializer):
-        obj=serializer.save(user=self.request.user)
-        return obj
+        '''
+        1.判断匿名 给定avatar
+        2.
+        '''
+        if (self.request.data.get("comment_status") == 1):
+            obj_1 = models.AskAnythingRecord.objects.filter(
+                user=self.request.user,
+                tacitrecord_id=self.request.data.get("tacitrecord"),
+                comment_status=1
+            ).order_by("id")
+            if obj_1.exists():
+                obj_1 = obj_1.first()
+                nickName=obj_1.nickName
+                avatarUrl=obj_1.avatarUrl
+            else:
+                nickName = getRandomName()
+                avatarUrl = getMosaic()
+            com_obj = serializer.save(user=self.request.user, nickName=nickName, avatarUrl=avatarUrl)
+            tacitrecord_id = serializer.data.get("tacitrecord")
+            models.TacitRecord.objects.filter(id=tacitrecord_id).update(comment_count=F('comment_count') + 1)
+        else:
+            obj_0 = models.AskAnythingRecord.objects.filter(
+                user=self.request.user,
+                tacitrecord_id=self.request.data.get("tacitrecord"),
+                comment_status=0
+            ).order_by("id")
+            if obj_0.exists():
+                obj_0_0 = obj_0.first()
+                nickName=obj_0_0.nickName
+                avatarUrl=obj_0_0.avatarUrl
+            else:
+                moment_obj = models.TacitRecord.objects.filter(id=self.request.data.get("tacitrecord")).first()
+                if self.request.user.id == moment_obj.user.id:
+                    nickName = moment_obj.user.real_nickName
+                    avatarUrl = moment_obj.user.real_avatarUrl
+                else:
+                    nickName, avatarUrl = getNameAvatarlist()
+            com_obj = serializer.save(user=self.request.user, nickName=nickName, avatarUrl=avatarUrl)
+            tacitrecord_id = serializer.data.get("tacitrecord")
+            models.TacitRecord.objects.filter(id=tacitrecord_id).update(comment_count=F('comment_count') + 1)
+
